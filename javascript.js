548ce6758bff0c5a7ec8185008f4c020a83b71de
nacho4017 <ivillanuevac@uc.cl>
2021-10-22 10:29:10 -0300

    1028

--- javascript.js
+++ javascript.js
@@ -20,35 +20,35 @@ function limpiar(){
 
 function ap(){
     document.getElementById("nombre_region").innerHTML = "Arica y Parinacota"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 32m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 32 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 25.680 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 299 viviendas"
 }
 
 function ta(){
     document.getElementById("nombre_region").innerHTML = "Tarapacá"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 46m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 46 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 60.927 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 359 viviendas"
 }
 
 function an(){
     document.getElementById("nombre_region").innerHTML = "Antofagasta"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 91m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 91 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 89.552 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 1.176 viviendas"
 }
 
 function at(){
     document.getElementById("nombre_region").innerHTML = "Atacama"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 91m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 91 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 16.070 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 449 viviendas"
 }
 
 function co(){
     document.getElementById("nombre_region").innerHTML = "Coquimbo"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 249m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 249 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 29.684 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 764 viviendas"
 }
@@ -62,21 +62,21 @@ function va(){
 
 function rm(){
     document.getElementById("nombre_region").innerHTML = "Metropolitana"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 2.477m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 2.477 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 762.875 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 2.324 viviendas"
 }
 
 function li(){
     document.getElementById("nombre_region").innerHTML = "O'Higgins"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 184m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 184 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 35.613 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 579 viviendas"
 }
 
 function ml(){
     document.getElementById("nombre_region").innerHTML = "Maule"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 248m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 248 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 35.137 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 767 viviendas"
 }
@@ -90,42 +90,42 @@ function nb(){
 
 function bi(){
     document.getElementById("nombre_region").innerHTML = "Biobío"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 427m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 427 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 26.929 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 1.159 viviendas"   
 }
 
 function ar(){
     document.getElementById("nombre_region").innerHTML = "La Araucania"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 359m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 359 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 18.645 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 778 viviendas" 
 }
 
 function lr(){
     document.getElementById("nombre_region").innerHTML = "Los Ríos"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 291m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 291 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 7.170 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 467 viviendas"     
 }
 
 function ll(){
     document.getElementById("nombre_region").innerHTML = "Los Lagos"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 154m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 154 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 20.825 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 1.034 viviendas" 
 }
 
 function ai(){
     document.getElementById("nombre_region").innerHTML = "Aysén"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 64m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 64 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 3.368 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 568 viviendas" 
 }
 
 function ma(){
     document.getElementById("nombre_region").innerHTML = "Magallanes y Antártica"
-    document.getElementById("areas_verdes").innerHTML = "Parques = 98m2"
+    document.getElementById("areas_verdes").innerHTML = "Parques = 98 ha"
     document.getElementById("pob_migrante").innerHTML = "Inmigrantes = 8.036 personas"
     document.getElementById("viv_colectivas").innerHTML = "Cantidad = 613 viviendas" 
 }
